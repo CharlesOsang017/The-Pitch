4b6fd37021186233ce913f146b6c7f22b9942f7b
CharlesOsang017 <charlesosango02@gmail.com>
2020-09-24 00:39:46 +0300

    style the login page

--- app/auth/views.py
+++ app/auth/views.py
@@ -15,7 +15,8 @@ def login():
             return redirect(request.args.get('next') or url_for('main.index'))
 
         flash('Invalid username or Password')
-    title = "Pitch login"
+    title = "Login"
+    
     return render_template('auth/login.html',login_form = login_form, title = title)
 
 @auth.route('/logout')
